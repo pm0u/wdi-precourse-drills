5c60dc2572d4f696d494cf6a752ab6333bea8133
Paul Mourer <paul.mourer@gmail.com>
2018-10-11 16:09:23 -0600

    deleted mustache code from style.css

--- script.js
+++ script.js
@@ -1,11 +1,7 @@
 $(document).ready(function() {
-	$("img.mustache").hide()
-	$(".checkbox").change(function() {
-		if(!this.checked) {
-			$("img.mustache").hide()
-		}
-		if(this.checked) {
-			$("img.mustache").show()
+		
+
+
 		}
 	})
 })
