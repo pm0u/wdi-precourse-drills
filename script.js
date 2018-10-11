dfcf757d869c43ad82c8968e311c31bf4d098b88
Paul Mourer <paul.mourer@gmail.com>
2018-10-11 16:50:28 -0600

    implemented some horrible jquery

--- script.js
+++ script.js
@@ -1,8 +1,46 @@
 $(document).ready(function() {
-		
+	$("div.content h1").mouseover(function () {
+		//console.log('moused over');
+		$(this).animate({fontSize: '100px'}, "slow");
 
-
-		}
 	})
+	$("div.content p").mouseover(function () {
+		//console.log('moused over');
+		$(this).animate({fontSize: '40px'}, "slow");
+
+	})
+	$("div.content h1").mouseout(function () {
+		//console.log('moused over');
+		$(this).animate({fontSize: '36px'}, "slow");
+
+	})
+	$("div.content p").mouseout(function () {
+		//console.log('moused over');
+		$(this).animate({fontSize: '16px'}, "slow");
+
+	})
+	$(".banner li").mouseover(function () {
+		//console.log('moused over');
+		$(this).animate({fontSize: '70px'}, "slow");
+
+	})
+	$(".banner li").mouseout(function () {
+		//console.log('moused over');
+		$(this).animate({fontSize: '20px'}, "slow");
+
+	})
+	$(".content img").mouseout(function () {
+		//console.log('moused over');
+		$(this).animate({width: '500px'}, "slow");
+
+	})
+	$(".content img").mouseover(function () {
+		//console.log('moused over');
+		$(this).animate({width: '1500px'}, "slow");
+
+	})
+
+
+
 })
 
